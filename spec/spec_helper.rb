dd6252c55d2a06d16f2fcf2fb47b81ed4b0de5e2
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-18 12:55:39 +0200

    * Added codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
+SimpleCov.start
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 require 'rimu'
