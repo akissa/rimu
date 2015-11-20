0a6e206963a5ed47e5cb4901569f8024382e2bfb
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-20 14:54:35 +0200

    * Included filter support * Remove domain name parameter from order method

--- lib/rimu/orders.rb
+++ lib/rimu/orders.rb
@@ -1,9 +1,17 @@
+require 'cgi'
+
 class Rimu::Orders < Rimu
-    def orders
-        send_request("/r/orders", "about_orders")
+    def orders(params={})
+        default_params = {
+            :include_inactive => 'N',
+            :server_type => 'VPS',
+        }
+        filters = prep_data(default_params, params)
+        path = "/r/orders;" + filters.collect {|k,v| "#{k}=#{CGI::escape(v.to_s)}"}.join(';')
+        send_request(path, "about_orders")
     end
 
-    def order(oid, domain)
-        send_request("/r/orders/order-#{oid}-#{domain}", "about_order")
+    def order(oid)
+        send_request("/r/orders/order-#{oid}-dn", "about_order")
     end
 end
