5dde47b7fdd8a8ae2e001255e159f97480e8f604
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-26 15:45:35 +0200

    * Updated tests

--- spec/rimu/servers_spec.rb
+++ spec/rimu/servers_spec.rb
@@ -4,7 +4,8 @@ require 'rimu'
 describe Rimu::Servers do
     before :each do
         @api_key = 'foo'
-        @rimu = Rimu::Servers.new(:api_key => @api_key)
+        @api_url = 'https://fake-api.rimuhosting.com'
+        @rimu = Rimu::Servers.new(:api_key => @api_key, :api_url => @api_url)
     end
 
     it 'should be a Rimu instance' do
@@ -134,30 +135,93 @@ describe Rimu::Servers do
             end
         end
     end
+    
     describe "when accessing the Rimu::Servers API change_state method" do
         it 'should require arguments' do
-            @rimu.stubs(:send_request)
-            lambda { @rimu.send(:change_state) }.should raise_error(ArgumentError)
+          @rimu.stubs(:send_request)
+          lambda { @rimu.send(:change_state) }.should raise_error(ArgumentError)
         end
         it 'should require an integer argument' do
-            @rimu.stubs(:send_request)
-            lambda { @rimu.send(:change_state, {}) }.should raise_error(ArgumentError)
+          @rimu.stubs(:send_request)
+          lambda { @rimu.send(:change_state, {}) }.should raise_error(ArgumentError)
         end
         it "should request the correct path" do
-            @rimu.expects(:send_request).with {|path, _, _, _| path == "/r/orders/order-10-dn/vps/running-state" }
-            @rimu.send(:change_state, 10, "RESTARTING")
+          @rimu.expects(:send_request).with {|path, _, _, _| path == "/r/orders/order-10-dn/vps/running-state" }
+          @rimu.send(:change_state, 10, "RESTARTING")
         end
         it "should request the correct field" do
-            @rimu.expects(:send_request).with {|_, field, _, _| field == "running_vps_info" }
-            @rimu.send(:change_state, 10, "RESTARTING")
+          @rimu.expects(:send_request).with {|_, field, _, _| field == "running_vps_info" }
+          @rimu.send(:change_state, 10, "RESTARTING")
         end
         it "should have the method parameter set" do
-            @rimu.expects(:send_request).with {|_, _, method, _| method == "PUT" }
-            @rimu.send(:change_state, 10, "RESTARTING")
+          @rimu.expects(:send_request).with {|_, _, method, _| method == "PUT" }
+          @rimu.send(:change_state, 10, "RESTARTING")
         end
         it "should have the data parameter set" do
-            @rimu.expects(:send_request).with {|_, _, _, data| data == {:running_state_change_request => {:running_state=>"RESTARTING"}} }
-            @rimu.send(:change_state, 10, "RESTARTING")
+          @rimu.expects(:send_request).with {|_, _, _, data| data == {:running_state_change_request => {:running_state=>"RESTARTING"}} }
+          @rimu.send(:change_state, 10, "RESTARTING")
         end
+    end
+    
+    describe 'when accessing the Rimu::Servers API move method' do
+      oid = 9999
+      it 'should require arguments' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:move) }.should raise_error(ArgumentError)
+      end
+      it 'should require an integer as the first argument' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:move, {}) }.should raise_error(ArgumentError)
+      end
+      it 'should require a hash as the second argument' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:move, oid, 1) }.should raise_error(ArgumentError)
+      end
+      it 'should request the correct path' do
+        @rimu.expects(:send_request).with {|path, _, _, _| path == "/r/orders/order-#{oid}-dn/vps/host-server" }
+        @rimu.send(:move, oid, {})
+      end
+      it 'should have the method parameter set' do
+        @rimu.expects(:send_request).with {|_, _, method, _| method == "PUT" }
+        @rimu.send(:move, oid, {})
+      end
+      it 'should have the data parameter set' do
+        _data = {:vps_move_request => {:update_dns=>false, :move_reason=>'', :pricing_change_option=>'CHOOSE_BEST_OPTION'}}
+        @rimu.expects(:send_request).with {|_, _, _, data| data == _data }
+        @rimu.send(:move, oid, {})
+      end
+    end
+    
+    describe 'when accessing the Rimu::Servers API resize method' do
+      oid = 9999
+      it 'should require arguments' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:resize) }.should raise_error(ArgumentError)
+      end
+      it 'should require an integer as the first argument' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:resize, {}) }.should raise_error(ArgumentError)
+      end
+      it 'should require a hash as the second argument' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:resize, oid, 1) }.should raise_error(ArgumentError)
+      end
+      it 'should require values in the hash argument' do
+        @rimu.stubs(:send_request)
+        lambda { @rimu.send(:resize, oid, {}) }.should raise_error(ArgumentError)
+      end
+      it 'should request the correct path' do
+        @rimu.expects(:send_request).with {|path, _, _, _| path == "/r/orders/order-#{oid}-dn/vps/parameters" }
+        @rimu.send(:resize, oid, {:memory_mb => 1024})
+      end
+      it 'should have the method parameter set' do
+        @rimu.expects(:send_request).with {|_, _, method, _| method == "PUT" }
+        @rimu.send(:resize, oid, {:memory_mb => 1024})
+      end
+      it 'should have the data parameter set' do
+        _data = {:vps_resize_request => {:memory_mb => 1024}}
+        @rimu.expects(:send_request).with {|_, _, _, data| data == _data }
+        @rimu.send(:resize, oid, {:memory_mb => 1024})
+      end
     end
 end
